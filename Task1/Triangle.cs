d8dcf28a36101b1a7e038e1799c77b46a2b78a5e
dikys <lrdikysa@mail.ru>
2016-09-27 00:57:38 +0500

    Add files via upload

---
+++ Task1/Triangle.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    public class Triangle
+    {
+        double[] angles = new double[3];
+        double[] sides = new double[3];
+        
+        public Triangle(double[] sides)
+        {
+            this.sides = sides;
+
+            this.angles[0] = CalculateAngleBeetwenBAndC(this.sides[1], this.sides[2], this.sides[0]);
+            this.angles[1] = CalculateAngleBeetwenBAndC(this.sides[2], this.sides[0], this.sides[1]);
+            this.angles[2] = CalculateAngleBeetwenBAndC(this.sides[0], this.sides[1], this.sides[2]);
+        }
+        public Triangle(double[] sides, double angle)
+        {
+            this.sides[0] = sides[0];
+            this.sides[1] = sides[1];
+
+            this.angles[2] = angle;
+
+            this.sides[2] = CalculateSideOppositeAngleThCos(this.angles[2], this.sides[0], this.sides[1]);
+            
+            this.angles[0] = CalculateAngleBeetwenBAndC(this.sides[1], this.sides[2], this.sides[0]);
+            this.angles[1] = CalculateAngleBeetwenBAndC(this.sides[2], this.sides[0], this.sides[1]);
+        }
+        public Triangle(double side, double[] angles)
+        {
+            this.sides[0] = side;
+
+            this.angles[2] = angles[0];
+            this.angles[1] = angles[1];
+            this.angles[0] = Math.PI - this.angles[1] - this.angles[2];
+
+            this.sides[1] = CalculateSideOppositeAngleThSin(this.angles[1], this.sides[0], this.angles[0]);
+            this.sides[2] = CalculateSideOppositeAngleThSin(this.angles[2], this.sides[0], this.angles[0]);
+        }
+        
+        double CalculateAngleBeetwenBAndC(double b, double c, double a)
+        {
+            return Math.Acos((b * b + c * c - a * a) / (2 * b * c));
+        }
+        // angle beetwen b and c
+        double CalculateSideOppositeAngleThCos(double angle, double b, double c)
+        {
+            return Math.Sqrt(b * b + c * c - 2 * b * c * Math.Cos(angle));
+        }
+        // sideA - adjacent side, angleA - angle opposite sideA
+        double CalculateSideOppositeAngleThSin(double angle, double sideA, double angleA)
+        {
+            return sideA * Math.Sin(angle) / Math.Sin(angleA);
+        }
+
+        public double GetSquare()
+        {
+            double semiperimeter = (this.sides[0] + this.sides[1] + this.sides[2]) / 2;
+
+            return Math.Sqrt(semiperimeter * (semiperimeter - this.sides[0]) * (semiperimeter - this.sides[1]) * (semiperimeter - this.sides[2]));
+        }
+        public double[] GetSides()
+        {
+            return this.sides;
+        }
+        public double[] GetAngles()
+        {
+            return this.angles;
+        }
+    }
+}
---
+++ Task1/TriangleTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Geometry.Test
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        double tolerance = 0.0001;
+
+        double[] sidesRightTriangle = new double[] { 3, 4, 5 };
+        double[] anglesRightTriangle = new double[] { Math.Asin(0.6), Math.Asin(0.8), Math.PI / 2 };
+        double squareRightTriangle = 6;
+
+        double[] sidesObtuseTriangle = new double[] { 9, 5, 6 };
+        double[] anglesObtuseTriangle = new double[] { 1.910633, 0.551285, 0.679673 };
+        double squareObtuseTriangle = 14.142135;
+
+        double[] sidesAcuteTriangle = new double[] { 4, 5, 6 };
+        double[] anglesAcuteTriangle = new double[] { 0.722734, 0.973389, 1.445468 };
+        double squareAcuteTriangle = 9.921567;
+
+        void CheckTriangle(Triangle triangle, double[] expectedSides, double[] expectedAngles, double expectedSquare)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Assert.IsTrue(Math.Abs(triangle.GetSides()[i] - expectedSides[i]) < tolerance);
+                Assert.IsTrue(Math.Abs(triangle.GetAngles()[i] - expectedAngles[i]) < tolerance);
+            }
+
+            Assert.IsTrue(Math.Abs(triangle.GetSquare() - expectedSquare) < tolerance);
+        }
+
+        [TestMethod]
+        public void InitializationTriangleAcrossThreeSides()
+        {
+            Triangle triangle = new Triangle(sidesRightTriangle);
+            CheckTriangle(triangle, sidesRightTriangle, anglesRightTriangle, squareRightTriangle);
+
+            triangle = new Triangle(sidesObtuseTriangle);
+            CheckTriangle(triangle, sidesObtuseTriangle, anglesObtuseTriangle, squareObtuseTriangle);
+
+            triangle = new Triangle(sidesAcuteTriangle);
+            CheckTriangle(triangle, sidesAcuteTriangle, anglesAcuteTriangle, squareAcuteTriangle);
+        }
+
+        [TestMethod]
+        public void InitializationTriangleAcrossTwoSidesAndAngle()
+        {
+            Triangle triangle = new Triangle(new double[] { sidesRightTriangle[0], sidesRightTriangle[1] }, anglesRightTriangle[2]);
+            CheckTriangle(triangle, sidesRightTriangle, anglesRightTriangle, squareRightTriangle);
+
+            triangle = new Triangle(new double[] { sidesObtuseTriangle[0], sidesObtuseTriangle[1] }, anglesObtuseTriangle[2]);
+            CheckTriangle(triangle, sidesObtuseTriangle, anglesObtuseTriangle, squareObtuseTriangle);
+
+            triangle = new Triangle(new double[] { sidesAcuteTriangle[0], sidesAcuteTriangle[1] }, anglesAcuteTriangle[2]);
+            CheckTriangle(triangle, sidesAcuteTriangle, anglesAcuteTriangle, squareAcuteTriangle);
+        }
+
+        [TestMethod]
+        public void InitializationTriangleAcrossSideAndTwoAngle()
+        {
+            Triangle triangle = new Triangle(sidesRightTriangle[0], new double[] { anglesRightTriangle[2], anglesRightTriangle[1] });
+            CheckTriangle(triangle, sidesRightTriangle, anglesRightTriangle, squareRightTriangle);
+
+            triangle = new Triangle(sidesObtuseTriangle[0], new double[] { anglesObtuseTriangle[2], anglesObtuseTriangle[1] });
+            CheckTriangle(triangle, sidesObtuseTriangle, anglesObtuseTriangle, squareObtuseTriangle);
+
+            triangle = new Triangle(sidesAcuteTriangle[0], new double[] { anglesAcuteTriangle[2], anglesAcuteTriangle[1] });
+            CheckTriangle(triangle, sidesAcuteTriangle, anglesAcuteTriangle, squareAcuteTriangle);
+        }
+        
+    }
+}
