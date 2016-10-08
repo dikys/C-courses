dea53da977127ccf1e305ea9eaae19411549c916
Vadim <lrdikysa@mail.ru>
2016-10-08 20:14:05 +0500

    Коммит

---
+++ Task 2/Task 2 Test/GameWithShiftHistoryTest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task_2;
+
+namespace Task_2_Test
+{
+    [TestClass]
+    public class GameWithShiftHistoryTest : GameTest
+    {
+        public new GameWithShiftHistory InitilizationCorrectGame()
+        {
+            return new GameWithShiftHistory(0, 1, 2, 3, 4, 5, 6, 7, 8);
+        }
+        
+        public new GameWithShiftHistory InitilizationGame(params int[] cells)
+        {
+            return new GameWithShiftHistory(cells);
+        }
+
+        [TestMethod]
+        public override void Should_RealValuesLocationEqualResultGetLocationFromItValue_When_AppliedShift()
+        {
+            var game = InitilizationCorrectGame();
+
+            game.Shift(1);
+
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    int value = x * 3 + y;
+                    Location valueLocation = game.GetLocation(value);
+
+                    Assert.AreEqual(x, valueLocation.X);
+                    Assert.AreEqual(y, valueLocation.Y);
+                }
+            }
+        }
+
+        [TestMethod]
+        public override void Should_RealValueEqualResultIndexerFromItValueLocation_When_AppliedShift()
+        {
+            var game = InitilizationCorrectGame();
+
+            game.Shift(1);
+
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    int value = x * 3 + y;
+
+                    Assert.AreEqual(value, game[x, y]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Should_RealValueEqualResultIndexerFromItValueLocationAtNewGame_When_AppliedShift()
+        {
+            var game = InitilizationCorrectGame();
+
+            game = game.Shift(1);
+
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    int value = x * 3 + y;
+
+                    if (value == 0)
+                    {
+                        value = 1;
+                    }
+                    else if (value == 1)
+                    {
+                        value = 0;
+                    }
+
+                    Assert.AreEqual(value, game[x, y]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Should_RealValuesLocationEqualResultGetLocationFromItValueByNewGame_When_AppliedShift()
+        {
+            var game = InitilizationCorrectGame();
+
+            var newGame = game.Shift(1);
+
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    int value = x * 3 + y;
+                    Location valueLocation = newGame.GetLocation(value);
+
+                    if (value == 0)
+                    {
+                        valueLocation = newGame.GetLocation(1);
+                    }
+                    else if (value == 1)
+                    {
+                        valueLocation = newGame.GetLocation(0);
+                    }
+
+                    Assert.AreEqual(x, valueLocation.X);
+                    Assert.AreEqual(y, valueLocation.Y);
+                }
+            }
+        }
+    }
+}
---
+++ Task 2/Task 2/GameWithShiftHistory.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public class GameWithShiftHistory : Game
+    {
+        int[] shiftHistory;
+
+        public GameWithShiftHistory(params int[] cells) : base(cells)
+        {
+            this.shiftHistory = new int[0];
+        }
+
+        public GameWithShiftHistory(int[] cells, int[] shiftHistory) : base(cells)
+        {
+            this.shiftHistory = shiftHistory;
+        }
+
+        public override int this[int x, int y]
+        {
+            get
+            {
+                int[] cells = new int[this.fieldSize];
+                for (int i = 0; i < this.field.GetLength(0); i++)
+                {
+                    for (int j = 0; j < this.field.GetLength(1); j++)
+                    {
+                        cells[i * this.field.GetLength(0) + j] = this.field[i, j];
+                    }
+                }
+
+                Game game = new Game(cells);
+
+                for (int i = 0; i < this.shiftHistory.Length; i++)
+                {
+                    game.Shift(this.shiftHistory[i]);
+                }
+
+                return game[x, y];
+            }
+        }
+
+        public override Location GetLocation(int value)
+        {
+            int[] cells = new int[this.fieldSize];
+            for (int x = 0; x < this.field.GetLength(0); x++)
+            {
+                for (int y = 0; y < this.field.GetLength(1); y++)
+                {
+                    cells[x * this.field.GetLength(0) + y] = this.field[x, y];
+                }
+            }
+
+            Game game = new Game(cells);
+
+            for (int i = 0; i < this.shiftHistory.Length; i++)
+            {
+                game.Shift(this.shiftHistory[i]);
+            }
+
+            return game.GetLocation(value);
+        }
+
+        public new GameWithShiftHistory Shift(int value)
+        {
+            if (value == 0 || !IsCorrectValue(value))
+                throw new ArgumentException("Value should be from 1 to " + (this.fieldSize - 1));
+
+            Location valueLocation = GetLocation(value);
+            Location zeroLocation = GetLocation(0);
+
+            if (Math.Abs(zeroLocation.X - valueLocation.X) + Math.Abs(zeroLocation.Y - valueLocation.Y) > 1)
+                throw new ArgumentException("Value can't move!");
+
+            int[] newShiftHistory = new int[this.shiftHistory.Length + 1];
+            for (int i = 0; i < this.shiftHistory.Length; i++)
+            {
+                newShiftHistory[i] = this.shiftHistory[i];
+            }
+            newShiftHistory[this.shiftHistory.Length] = value;
+
+            int[] cells = new int[this.fieldSize];
+            for (int x = 0; x < this.field.GetLength(0); x++)
+            {
+                for (int y = 0; y < this.field.GetLength(1); y++)
+                {
+                    cells[x * this.field.GetLength(0) + y] = this.field[x, y];
+                }
+            }
+
+            GameWithShiftHistory result = new GameWithShiftHistory(cells, newShiftHistory);
+
+            return result;
+        }
+    }
+}
