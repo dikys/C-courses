9216f68f89750ae9dc89401e6190f389db49f3b1
dikys <lrdikysa@mail.ru>
2016-10-11 15:16:52 +0300

    Create SpellOnTarget.cs

---
+++ Task 3/Task 3/Spells/SpellOnTarget.cs
@@ -0,0 +1,11 @@
+
+namespace Task_3.Spells
+{
+    public abstract class SpellOnTarget : Spell, ISpellOnTarget
+    {
+        public bool ApplySpell(ICharacter target)
+        {
+
+        }
+    }
+}
