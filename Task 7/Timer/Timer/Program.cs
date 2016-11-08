ba19e74589e4ce15038b908a5b7a72f1986bfdbd
dikys <lrdikysa@mail.ru>
2016-11-08 21:15:12 +0500

    Первое задание

---
+++ Task 7/Timer/Timer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Timer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new Timer();
+            using (timer.Start())
+            {
+                for (var n = 0; n < 1000000; n++)
+                {
+                    var j = n + 1;
+                }
+            }
+            Console.WriteLine(timer.ElapsedMilliseconds);
+
+            using (timer.Continue())
+            {
+                for (var n = 0; n < 1000000; n++)
+                {
+                    var j = n + 1;
+                }
+            }
+            Console.WriteLine(timer.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ Task 7/Timer/Timer/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Timer
+{
+    public class Timer : IDisposable
+    {
+        private Stopwatch stopwatch;
+
+        public long ElapsedMilliseconds
+        {
+            get { return this.stopwatch.ElapsedTicks; }
+        }
+
+        public Timer()
+        {
+            stopwatch = new Stopwatch();
+        }
+
+        public void Dispose()
+        {
+            this.stopwatch.Stop();
+        }
+
+        public Timer Start()
+        {
+            this.stopwatch.Reset();
+            this.stopwatch.Start();
+
+            return this;
+        }
+
+        public Timer Continue()
+        {
+            this.stopwatch.Start();
+
+            return this;
+        }
+    }
+}
