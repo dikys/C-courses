f51016466ec47979c7faa942c8c76958933a3532
dikys <lrdikysa@mail.ru>
2016-11-11 00:40:22 +0500

    2 задание

---
+++ Task 7/BitmapEditor/BitmapEditor/BitmapEditor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace BitmapEditor
+{
+    public class BitmapEditor : IDisposable
+    {
+        private bool isDisposed = false;
+        public Bitmap Image { get; }
+
+        public BitmapEditor(Bitmap image)
+        {
+            this.Image = image;
+        }
+
+        public void SetPixel(int x, int y, byte R, byte G, byte B)
+        {
+            var lockableBox = new Rectangle(x, y, 1, 1);
+
+            var imageDate = this.Image.LockBits(lockableBox, ImageLockMode.ReadWrite, this.Image.PixelFormat);
+
+            IntPtr ptr = imageDate.Scan0;
+
+            byte[] pixelColor = new byte[3];
+
+            Marshal.Copy(ptr, pixelColor, 0, 3);
+
+            pixelColor[0] = R;
+            pixelColor[1] = G;
+            pixelColor[2] = B;
+
+            Marshal.Copy(pixelColor, 0, ptr, 3);
+
+            this.Image.UnlockBits(imageDate);
+        }
+
+        ~BitmapEditor()
+        {
+            this.Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (isDisposed)
+                return;
+
+            this.isDisposed = true;
+
+            this.Image.Dispose();
+        }
+    }
+}
---
+++ Task 7/BitmapEditor/BitmapEditor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BitmapEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bitmap = new Bitmap("avos.jpg");
+
+            using (var imageEditor = new BitmapEditor(bitmap))
+            {
+                for (int x = 0; x < bitmap.Width / 2; x++)
+                    for (int y = 0; y < bitmap.Height / 2; y++)
+                        imageEditor.SetPixel(x, y, 0, 0, 0);
+
+                var form = new Form();
+
+                form.Width = imageEditor.Image.Width;
+                form.Height = imageEditor.Image.Height;
+
+                form.BackgroundImage = imageEditor.Image;
+
+                Application.Run(form);
+            }
+        }
+    }
+}
